e77a90595ad60ce7d2d4405be7c854c7e2fc01ed
ME_Kun_Han <hanvskun@hotmail.com>
2016-02-16 20:28:32 +0800

    Init program

---
+++ sort/sort/BaseSort.h
@@ -0,0 +1,12 @@
+#pragma once
+// this is the base sort 
+// try to use template
+
+class CBaseSort
+{
+public:
+	CBaseSort();
+	virtual ~CBaseSort();
+
+};
+
---
+++ sort/sort/sort.cpp
@@ -0,0 +1,9 @@
+#include "common.h"
+
+// the entrance of program
+int main(void)
+{
+	printf_s("hello world!\n");
+	getchar();
+	return 0;
+}
